9c67f926164610d91917f5defa773a10c0aa2474
konosuke115 <algas115@gmail.com>
2020-02-19 23:00:12 +0900

    separated components (EventForm, EventList)

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,6 @@ import React from 'react'
 //   const dispatch = props.dispatch
 //   const event = props.event
 const Event = ({ dispatch, event }) => {
-  //
   const handleClickDeleteButton = () => {
     const result = window.confirm(
       `イベントID:${event.id}を本当に削除しても宜しいでしょうか？`
@@ -30,28 +29,3 @@ const Event = ({ dispatch, event }) => {
 }
 
 export default Event
-
-// {state.map((event, index) => {
-//   const handleClickDeleteButton = () => {
-//     dispatch({
-//       type: 'DELETE_EVENT',
-//       id: event.id
-//     })
-//   }
-//   return (
-//     <tr key={index}>
-//       <td>{event.id}</td>
-//       <td>{event.title}</td>
-//       <td>{event.body}</td>
-//       <td>
-//         <button
-//           type="button"
-//           className="btn btn-danger"
-//           onClick={handleClickDeleteButton}
-//         >
-//           削除
-//         </button>
-//       </td>
-//     </tr>
-//   )
-// })}
